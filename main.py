a1b553c41388202d054c8f1fae428ec490a9d3c3
Sminth <emmanuelmalan@gmail.com>
2022-08-19 19:24:48 +0000

    MAJ

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ Ce projet consiste a afficher la meteo d'une localité
 print ('Veuillez entrer la ville dont la météo est demandé') 
 ville = input('Entrer la ville > ').lower() #je recupere la ville 
 
-Dico= {"abidjan":100, "daloa":45}
+with open("villes.txt","r") as f :
+    Dico = eval(f.read())
+
 
 if ville in Dico.keys() :
     print("la temperature a {} est de : {} degre".format(ville, Dico[ville]))
