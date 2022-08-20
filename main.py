dbd6c8269f08eb672a75fa62d9b364edcd853666
DxdxO <aude.oka@gmail.com>
2022-08-19 19:36:36 -0700

    maj

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ Ce projet consiste a afficher la meteo d'une localité
 print ('Veuillez entrer la ville dont la météo est demandé') 
 ville = input('Entrer la ville > ').lower() #je recupere la ville 
 
+#ouverture du fichier villes.txt (f)
 with open("villes.txt","r") as f :
-    Dico = eval(f.read())
+    Dico = eval(f.read()) #eval renvoi le vrai type de la chaine de caratere retourné a="2"; b=eval(a)
 
 
 if ville in Dico.keys() :
